4e3b9ad1157390f64ffb13d5f5f53d5f020455a1
tomashco <tommaso@ndg.ai>
2025-05-29 21:02:43 +0000

    feat: addition of ratings, demographics and others

--- lib/pento/catalog/product.ex
+++ lib/pento/catalog/product.ex
@@ -2,13 +2,17 @@ defmodule Pento.Catalog.Product do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Pento.Survey.Rating
+
   schema "products" do
     field :name, :string
     field :description, :string
     field :unit_price, :float
     field :sku, :integer
 
-    timestamps(type: :utc_datetime)
+    timestamps()
+
+    has_many :ratings, Rating
   end
 
   @doc false
@@ -17,5 +21,6 @@ defmodule Pento.Catalog.Product do
     |> cast(attrs, [:name, :description, :unit_price, :sku])
     |> validate_required([:name, :description, :unit_price, :sku])
     |> unique_constraint(:sku)
+    |> validate_number(:unit_price, greater_than: 0.0)
   end
 end
